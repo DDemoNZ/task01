da865274984f997cf08af87b30ae5939dd898389
Dmytro Zinchuk1 <Dmytro.Zinchuk@ge.com>
2024-01-17 00:48:10 +0200

    try to fix function url issue

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -10,22 +10,22 @@ import com.syndicate.deployment.model.lambda.url.InvokeMode;
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}"
+@LambdaHandler(
+        lambdaName = "hello_world",
+        roleName = "hello_world-role",
+        isPublishVersion = true
 )
 @LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
-	}
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        System.out.println("Hello from lambda");
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("statusCode", 200);
+        resultMap.put("message", "Hello from Lambda");
+        return resultMap;
+    }
 }
